74cc2e8d3b48c32dfd859ca1211eee9f13b1765a
Ezra Bruggeman <42326863+ezrabru@users.noreply.github.com>
2022-10-12 22:34:41 +0100

    Implement hsv map rendering

--- src/napari_polcam/_widget.py
+++ src/napari_polcam/_widget.py
@@ -9,6 +9,8 @@ Replace code below according to your needs.
 import numpy as np
 import napari
 
+from matplotlib.colors import hsv_to_rgb
+
 from vispy.color import Colormap
 
 from typing import TYPE_CHECKING
@@ -29,14 +31,14 @@ class StokesQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
         
-        btn_s0 = QPushButton("Set S0")
-        btn_s0.clicked.connect(self._on_click_s0)
+        #btn_s0 = QPushButton("Set S0")
+        #btn_s0.clicked.connect(self._on_click_s0)
         
-        btn_s1 = QPushButton("Set S1")
-        btn_s1.clicked.connect(self._on_click_s1)
+        #btn_s1 = QPushButton("Set S1")
+        #btn_s1.clicked.connect(self._on_click_s1)
         
-        btn_s2 = QPushButton("Set S2")
-        btn_s2.clicked.connect(self._on_click_s2)
+        #btn_s2 = QPushButton("Set S2")
+        #btn_s2.clicked.connect(self._on_click_s2)
         
         btn_aolp = QPushButton("Calculate AoLP")
         btn_aolp.clicked.connect(self._on_click_aolp)
@@ -44,12 +46,16 @@ class StokesQWidget(QWidget):
         btn_dolp = QPushButton("Calculate DoLP")
         btn_dolp.clicked.connect(self._on_click_dolp)
         
+        btn_hsv_map = QPushButton("Calculate HSVmap")
+        btn_hsv_map.clicked.connect(self._on_click_hsvmap)
+        
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn_s0)
-        self.layout().addWidget(btn_s1)
-        self.layout().addWidget(btn_s2)
+        #self.layout().addWidget(btn_s0)
+        #self.layout().addWidget(btn_s1)
+        #self.layout().addWidget(btn_s2)
         self.layout().addWidget(btn_aolp)
         self.layout().addWidget(btn_dolp)
+        self.layout().addWidget(btn_hsv_map)
 
     def _on_click_s0(self):
         num_layers = 0
@@ -105,6 +111,37 @@ class StokesQWidget(QWidget):
         s2 = self.viewer.layers['S2'].data
         DoLP = np.sqrt((s1*s1 + s2*s2)/(s0*s0))
         self.viewer.add_image(DoLP,contrast_limits=[0,1])
+    
+    def _on_click_hsvmap(self):
+        h = self.viewer.layers['AoLP'].data
+        s = self.viewer.layers['DoLP'].data
+        v = self.viewer.layers['S0'].data
+        
+        h = (h + np.pi/2)/np.pi; # rescale [-pi/2, pi/2]to [0, 1]
+        v = (v - np.min(v))/(np.max(v) - np.min(v)); # rescale intensity to [0 1]
+        
+        
+        numDim = len(h.shape) # number of dimensions of the dataset
+        hsv = np.stack([h,s,v],numDim) # stack
+        rgb = hsv_to_rgb(hsv)
+        
+        hsv_r = rgb[..., 0]*255
+        hsv_g = rgb[..., 1]*255
+        hsv_b = rgb[..., 2]*255
+        
+        hsv_r = hsv_r.astype(np.uint8)
+        hsv_g = hsv_g.astype(np.uint8)
+        hsv_b = hsv_b.astype(np.uint8)
+        
+        # hide all layers
+        #for layer in self.viewer.layers.selection:
+        #    self.viewer.layers[layer].selected = False
+        
+        # add new images for the 3 colour channels
+        self.viewer.add_image(hsv_r,contrast_limits=[0,255],colormap="red",blending="additive")
+        self.viewer.add_image(hsv_g,contrast_limits=[0,255],colormap="green",blending="additive")
+        self.viewer.add_image(hsv_b,contrast_limits=[0,255],colormap="blue",blending="additive")
+        
         
     def selected_image_layers(self):
         return [
