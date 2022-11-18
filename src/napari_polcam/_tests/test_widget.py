2801134dab95457db16e5273ee65ea20245497ee
ezra bruggeman <ezra.bruggeman@gmail.com>
2022-11-18 21:35:09 +0000

    Edit test_widget.py

--- src/napari_polcam/_tests/test_widget.py
+++ src/napari_polcam/_tests/test_widget.py
@@ -1,18 +1,24 @@
-import numpy as np
+#import numpy as np
 
-from napari_polcam import StokesQWidget
+#from napari_polcam import StokesQWidget
 
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
 
-    # create our widget, passing in the viewer
-    try:
-        my_widget = StokesQWidget(viewer)
-        return True
-    except:
-        return False
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 6, "Should be 6"
+
+## make_napari_viewer is a pytest fixture that returns a napari viewer object
+## capsys is a pytest fixture that captures stdout and stderr output streams
+#def test_example_q_widget(make_napari_viewer, ):
+#    # make viewer and add an image layer using our fixture
+#    viewer = make_napari_viewer()
+#    viewer.add_image(np.random.random((100, 100)))
+#
+#    # create our widget, passing in the viewer
+#    try:
+#        my_widget = StokesQWidget(viewer)
+#        return True
+#    except:
+#        return False
